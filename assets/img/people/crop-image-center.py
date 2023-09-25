5cdc6e0b2109437b62a574dcb9001fb869d923d0
Max Parzen <max.parzen@ed.ac.uk>
2023-09-25 10:58:28 +0200

    add resize function and fix emmanuel

--- assets/img/people/crop-image-center.py
+++ assets/img/people/crop-image-center.py
@@ -3,28 +3,32 @@
 """
 Created on Tue Oct 19 15:08:48 2021
 
-@author: fabian
+@author: fabian hofmann + max parzen
 """
 
 from PIL import Image
 import os
 
-def centercrop(path):
+def centercrop_and_resize(path, min_size=274):
     im = Image.open(path)
-    width, height = im.size   # Get dimensions
+    width, height = im.size  # Get dimensions
 
+    # Crop to a square
     new_width = new_height = min(height, width)
-
     left = (width - new_width)/2
     top = (height - new_height)/2
     right = (width + new_width)/2
     bottom = (height + new_height)/2
+    im_cropped = im.crop((left, top, right, bottom))
 
-    # Crop the center of the image
-    return im.crop((left, top, right, bottom))
-
+    # Resize the image if it's smaller than the specified minimum size
+    if new_width < min_size or new_height < min_size:
+        im_resized = im_cropped.resize((min_size, min_size), Image.LANCZOS)
+        return im_resized
+    
+    return im_cropped
 
 for path in ['assets/img/people/emmanuel.jpg']:
     name = path.split('.')[0]
     path = os.path.join(os.getcwd(), path)
-    centercrop(path).save(name+'_cropped.jpg')
+    centercrop_and_resize(path).save(name+'_cropped.jpg')
