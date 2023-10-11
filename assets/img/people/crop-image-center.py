4613d400635e915bd4918201231d4a75f261e395
BryanFran <brramirez@unal.edu.co>
2023-10-11 21:29:09 +0200

    add bryan

--- assets/img/people/crop-image-center.py
+++ assets/img/people/crop-image-center.py
@@ -9,26 +9,28 @@ Created on Tue Oct 19 15:08:48 2021
 from PIL import Image
 import os
 
+
 def centercrop_and_resize(path, min_size=274):
     im = Image.open(path)
     width, height = im.size  # Get dimensions
 
     # Crop to a square
     new_width = new_height = min(height, width)
-    left = (width - new_width)/2
-    top = (height - new_height)/2
-    right = (width + new_width)/2
-    bottom = (height + new_height)/2
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
     im_cropped = im.crop((left, top, right, bottom))
 
     # Resize the image if it's smaller than the specified minimum size
     if new_width < min_size or new_height < min_size:
         im_resized = im_cropped.resize((min_size, min_size), Image.LANCZOS)
         return im_resized
-    
+
     return im_cropped
 
-for path in ['assets/img/people/emmanuel.jpg']:
+
+for path in [r"C:\Users\ramir\OneDrive\Escritorio\OET\EOT_Page\oet-website\assets\img\people\ramirezbryan.jpg,"r"]:
     name = path.split('.')[0]
     path = os.path.join(os.getcwd(), path)
-    centercrop_and_resize(path).save(name+'_cropped.jpg')
+    centercrop_and_resize(path).save(name + '_cropped.jpg')
