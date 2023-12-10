fa1149c2b0e590a99e9a4d24b063aed54f2f0645
Oluwafemi Abiona <68615038+Olufems@users.noreply.github.com>
2023-12-10 01:01:44 +0100

    Update crop-image-center.py

--- assets/img/people/crop-image-center.py
+++ assets/img/people/crop-image-center.py
@@ -30,7 +30,7 @@ def centercrop_and_resize(path, min_size=274):
     return im_cropped
 
 
-for path in ["assets/img/people/oluwafemi.jpg"]:
+for path in ["assets/img/people/oluwafemi.jpeg"]:
     name = path.split('.')[0]
     path = os.path.join(os.getcwd(), path)
     centercrop_and_resize(path).save(name + '_cropped.jpg')
